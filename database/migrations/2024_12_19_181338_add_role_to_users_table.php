d2a85a9c13283539f7d684d7931b7e9e89838831
JesusLima8 <145725254+JesusLima8@users.noreply.github.com>
2024-12-31 17:47:28 -0500

    rol

--- database/migrations/2024_12_19_181338_add_role_to_users_table.php
+++ database/migrations/2024_12_19_181338_add_role_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role1')->default('user'); // Agrega la columna "role" con valor por defecto "user"
+            $table->string('role')->default('user'); // Agrega la columna "role" con valor por defecto "user"
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role1'); // Elimina la columna "role" al revertir la migración
+            $table->dropColumn('role'); // Elimina la columna "role" al revertir la migración
         });
     }
 };
