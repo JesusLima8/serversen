c2b2120b5c81743488635bac3ee1e42c4eb8cd26
JesusLima8 <145725254+JesusLima8@users.noreply.github.com>
2024-12-31 17:47:46 -0500

    rol

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('user'); // Agregado el campo de rol con valor predeterminado 'user'
+           // $table->string('role')->default('user'); // Agregado el campo de rol con valor predeterminado 'user'
             $table->rememberToken();
             $table->timestamps();
         });
